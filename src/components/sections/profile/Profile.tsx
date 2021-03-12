4455ff34b35147f99cd7f5597ea712bfa51f5c21
Marius Franzén <marius.franzen1@gmail.com>
2021-03-12 14:34:17 +0100

    added image

--- src/components/sections/profile/Profile.tsx
+++ src/components/sections/profile/Profile.tsx
@@ -1,15 +1,14 @@
 import { Paper } from '@material-ui/core'
 import React, { ReactElement } from 'react'
-import * as Selfie from "../../../static/images/IMG_1253.15775759.jpg"
 
-interface Props {
+interface Props { 
     
 }
 
 function Profile({}: Props): ReactElement {
     return (
         <Paper>
-            <img src={Selfie} />
+            <img src="../../../../static/images/IMG_1253.15775759.jpg" />
         </Paper>
     )
 }
